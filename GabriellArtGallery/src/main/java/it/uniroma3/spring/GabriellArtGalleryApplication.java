fe10a1f7916eb3f9fd607a9e3416017b1852355c
tommaso caiazzi <tommasocaiazzi@gmail.com>
2017-06-11 23:20:55 +0200

    commit iniziale

---
+++ GabriellArtGallery/src/main/java/it/uniroma3/spring/GabriellArtGalleryApplication.java
@@ -0,0 +1,12 @@
+package it.uniroma3.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabriellArtGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabriellArtGalleryApplication.class, args);
+	}
+}
