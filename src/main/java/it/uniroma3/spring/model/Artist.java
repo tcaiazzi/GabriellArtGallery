c79b0e7e01794c35f2e6ce116d2614d2b0a6fb3c
tommaso caiazzi <tommasocaiazzi@gmail.com>
2017-06-12 13:24:17 +0200

    prime luci del login

--- src/main/java/it/uniroma3/spring/model/Artist.java
+++ src/main/java/it/uniroma3/spring/model/Artist.java
@@ -34,6 +34,63 @@ public class Artist {
 		// TODO Auto-generated constructor stub
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Date getDeathDate() {
+		return deathDate;
+	}
+
+	public void setDeathDate(Date deathDate) {
+		this.deathDate = deathDate;
+	}
+
+	public Map<String, Picture> getPictures() {
+		return pictures;
+	}
+
+	public void setPictures(Map<String, Picture> pictures) {
+		this.pictures = pictures;
+	}
+	
+	
 
 	
 	
