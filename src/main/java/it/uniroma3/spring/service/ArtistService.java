05cdfaf37e9b587098aadc07f62a43f11a530b07
Lerry <valeriocaravani@gmail.com>
2017-06-12 13:24:40 +0200

    commit inserimento Artista

---
+++ src/main/java/it/uniroma3/spring/service/ArtistService.java
@@ -0,0 +1,24 @@
+package it.uniroma3.spring.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.uniroma3.spring.model.Artist;
+
+import it.uniroma3.spring.repository.ArtistRepository;
+
+
+@Service
+public class ArtistService{
+	
+	@Autowired
+	private ArtistRepository artistRepository;
+	
+	@Transactional
+	public void add(final Artist artist){
+		this.artistRepository.save(artist);
+	}
+	
+
+}
